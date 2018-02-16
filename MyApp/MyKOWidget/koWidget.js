c91a92dcb94dcc8f5a7e41f0f56409513404efc3
Ravindra Singh <shekhawatravi@users.noreply.github.com>
2018-02-16 16:22:45 +0300

    Creating View Model as Module in Seperate JS File
    Tested by Creating Seperate ViewModel in a dojo Module.

--- MyApp/MyKOWidget/koWidget.js
+++ MyApp/MyKOWidget/koWidget.js
@@ -1,24 +1,17 @@
-require([
-    "dojo/_base/declare", 
-    "dojo/parser", 
-    "dijit/_WidgetBase",
-    "dijit/_TemplatedMixin",
-    "dojo/text! MyApp/MyKOWidget/koWidgetTemplate.html",
-    "knockout",
-    "dojo/domReady",
-], function(declare, parser, _WidgetBase, _TemplatedMixin, thisWidgetTemplate, ko, eady){
-
-    declare("koWidget", [_WidgetBase, _TemplatedMixin], {
-        // put methods, attributes, etc. here
-
-        templateString: thisWidgetTemplate,
-
-        postCreate: function(){
-            // System Function
+define( function(){
+    var privateValue = 0;
+    return{
+        increment: function(){
+            privateValue++;
         },
-    });
-    ready(function(){
-        // Call the parser manually so it runs after our widget is defined, and page has finished loading
-        parser.parse();
-    });
+
+        decrement: function(){
+            privateValue--;
+        },
+
+        getValue: function(){
+            return privateValue;
+        }
+    }
+
 });
